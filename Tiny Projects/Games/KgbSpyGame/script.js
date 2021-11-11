971e0f537051e4083568b34330e6836d800d0dfd
Ida L <83402895+Ida-1@users.noreply.github.com>
2021-11-11 08:48:08 +0100

    Add files via upload

---
+++ Tiny Projects/Games/KgbSpyGame/script.js
@@ -0,0 +1,37 @@
+function enemyAttacksMe(enemy) {
+
+    enemy.classList.add("showing");
+
+    setTimeout( ()=> {
+        enemyShootsMe(enemy);
+    }, 1000);
+
+    setTimeout( ()=> {
+        enemy.classList.remove("showing");
+    }, 3000);
+}
+
+function enemyShootsMe(enemy) {
+    enemy.classList.add("shooting");
+
+    setTimeout( ()=> {
+        enemy.classList.remove("shooting");
+    }, 200);
+}
+
+function livingEnemies(){
+
+    return document.querySelectorAll(".enemy:not(.dead)");
+}
+
+function randomEnemyAttacks() {
+    var randomEnemyNo = Math.random()* livingEnemies().length;
+    randomEnemyNo = Math.floor(randomEnemyNo);
+    var enemy = livingEnemies()[randomEnemyNo];
+
+    var randomDelay = Math.random() * 2000 + 1000;
+
+    setTimeoout( ()=> {
+        enemyAttacksMe(enemy);
+    }, randomDelay);
+}
